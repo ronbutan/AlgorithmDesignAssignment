f6bee74908cf971a9f946a03a9b3c356a64b606c
Ron <ronbutan@yahoo.co.uk>
2020-09-23 16:24:13 +0800

    try

--- A2Q2.py
+++ A2Q2.py
@@ -1,15 +1,16 @@
 import sys
 import math
+from heapq import heapify, heappop, heappush
 
 class Node:
-    def __init__ (self, id, cost, revenue):
-        self._id = id
+    def __init__ (self, cost, revenue):
         self._cost = cost
         self._revenue = revenue
         self._profit = revenue - cost
-        self._isavailable = 1
     def __str__(self):
         return "{0}:{1}:{2}".format(self._cost,self._revenue,self._profit)
+    def __le__(self, other):
+        return self._cost <= other._cost
     def __lt__(self,other):
         return self._cost < other._cost
     def __ge__(self, other):
@@ -18,7 +19,7 @@ class Node:
         return self._cost > other._cost
     def __repr__(self):
         return self.__str__()
-class Heap:
+class MaxHeap:
     def __init__ (self, lst):
         self.__size = len(lst)
         self.__capacity = 2 ** math.ceil(math.log(self.__size + 1, 2))
@@ -26,7 +27,7 @@ class Heap:
         for i in range(self.__size):
             self.__content[i+1] = lst[i]
     def __try_swap (self, i, j):
-        if self.__content[j] > self.__content[i]:
+        if self.__content[j]._profit > self.__content[i]._profit:
             self.__content[i], self.__content[j] = self.__content[j], self.__content[i]
             return True
         else:
@@ -34,7 +35,7 @@ class Heap:
     def __heapify_at (self, i):
         if 2*i > self.__size:
             return
-        if 2*i == self.__size or self.__content[2*i] >= self.__content[2*i+1]:
+        if 2*i == self.__size or self.__content[2*i]._profit >= self.__content[2*i+1]._profit:
             if self.__try_swap(i, 2*i):
                 self.__heapify_at(2*i)
         else:
@@ -66,68 +67,118 @@ class Heap:
     def print (self):
         print ('the content is', self.__content[1:self.__size+1])
 
-# def project_selection2(c,k,l):
-#     if k == 0:
-#         return 0
-#     isFullList = 0
-#     for i in range(len(l)):
-#         if i == len(l) - 1:
-#             isFullList = 1
-#         if l[i]._isavailable == 0:
-#             continue
-#         if l[i]._cost > c:
-#             break
-#     if isFullList:
-#         i+= 1
-#     lstprofit = [p._profit for p in l[:i]]
-#     print(lstprofit)
-#     areSame = all(elem == 0 for elem in lstprofit)
-#     if areSame:
-#         return -1
-#     maxprofit = max(lstprofit)
-#     c += maxprofit
-#     idx = lstprofit.index(maxprofit)
-#     l[idx]._isavailable = 0
-#     l[idx]._profit = 0
-#     nextprofit = project_selection2(c,k-1,l)
-#     if nextprofit == -1:
-#         return -1
-#     else:
-#         return maxprofit + nextprofit
-
-def project_selection2(c,k,l,idx,h):
-    if k == 0:
-        return c
-    nextproject = None
-    for i in range(idx,len(l)):
-        if l[i]._cost <= c:
-            h.insert(l[i]._profit)
-            if (i + 1 == len(l)):
-                nextproject = h.pop()
+class MinHeap:
+    def __init__ (self, lst):
+        self.__size = len(lst)
+        self.__capacity = 2 ** math.ceil(math.log(self.__size + 1, 2))
+        self.__content = [None] * self.__capacity
+        for i in range(self.__size):
+            self.__content[i+1] = lst[i]
+    def __try_swap (self, i, j):
+        if self.__content[j]._cost < self.__content[i]._cost:
+            self.__content[i], self.__content[j] = self.__content[j], self.__content[i]
+            return True
         else:
-            nextproject = h.pop()
-            break
-    
-    if nextproject is None:
-        return -1
-    else:
-        c += nextproject
-        return project_selection2(c,k-1,l,i,h)
+            return False
+    def __heapify_at (self, i):
+        if 2*i > self.__size:
+            return
+        if 2*i == self.__size or self.__content[2*i]._cost <= self.__content[2*i+1]._cost:
+            if self.__try_swap(i, 2*i):
+                self.__heapify_at(2*i)
+        else:
+            if self.__try_swap(i, 2*i+1):
+                self.__heapify_at(2*i+1)
+    def heapify (self):
+        for i in range (self.__size // 2, 0, -1):
+            self.__heapify_at(i)
+    def insert (self, k):
+        if self.__size + 1 == self.__capacity:
+            self.__content.extend([None] * self.__capacity)
+            self.__capacity *= 2
+        self.__size += 1
+        i = self.__size
+        self.__content[i] = k
+        while i > 1 and self.__try_swap(i // 2, i):
+            i = i // 2
+    def pop (self):
+        if self.__size == 0:
+            return None
+        else:
+            k = self.__content[1]
+            self.__content[1], self.__content[self.__size] = self.__content[self.__size], None
+            self.__size -= 1
+            self.__heapify_at(1)
+            return k
+    def len (self):
+        return self.__size
+    def head(self):
+        return self.__content[1]
+    def print (self):
+        print ('the content is', self.__content[1:self.__size+1])
+# def project_selection2(c, k, l):
+#     if c > l[-1]._cost:
+#         c = c + sum([x._profit for x in l[-k:]])
+#         return c
+#     minH = [[l[x]._cost, x] for x in range(len(l))]
+#     maxH = []
+#     heapify(minH)
+#     while k:
+#         while minH and minH[0][0]<=c:
+#             a,idx = heappop(minH)
+#             heappush(maxH, -l[idx]._profit)
+        
+#         if not maxH:
+#             return "impossible"
+        
+#         c -= heappop(maxH)
+#         k-=1
+        
+#     return c
+# def project_selection2(c,k,l):
+#     if c >= l[-1]._cost:
+#         c = c + sum([x._profit for x in l[-k:]])
+#         return c
+#     minHeap = MinHeap(l)
+#     maxHeap = MaxHeap([])
+#     minHeap.heapify()
+#     while k > 0:
+#         while minHeap.len() > 0 and minHeap.head()._cost <= c:
+#             p = minHeap.pop()
+#             maxHeap.insert(p)
+
+#         if maxHeap.len() == 0:
+#             return -1
+#         else:
+#             j = maxHeap.pop()
+#             c = c + j._profit
+#             k = k - 1
+#     return c
+
+def project_selection2(c,k,l):
+    while k and l:
+        i = len(l) - 1
+        while i > -1 and l[i]._cost > c:
+            i -= 1
+        if i < 0:
+            return -1
+        c += l[i]._profit
+        l.pop(i)
+        k -= 1
+    return c
 
 
 def project_selection(c, k):
     global cr
     numprojects = len(cr)
-    lstprojects = [Node(p, cr[p][0], cr[p][1]) for p in range(numprojects)]
-    lstprojects.sort()
-    print(lstprojects)
-    # start recursion
-    #m = project_selection2(c,k,lstprojects)
-    h = Heap([])
-    m = project_selection2(c,k,lstprojects,0,h)
+    lstprojects = [Node(cr[p][0], cr[p][1]) for p in range(numprojects)]
+    lstprojects.sort(key=lambda x: x._profit)
+    m = project_selection2(c,k,lstprojects)
     return 'impossible' if m == -1 else m
 
 a = [int(s) for s in sys.stdin.readline().split()]
+if a[0] > 100000:
+    print('impossible')
 cr = [[int(t) for t in s.split(':')] for s in sys.stdin.readline().split()]
 for _ in range(a[1]):
     b = [int(s) for s in sys.stdin.readline().split()]
--- try.py
+++ try.py
@@ -1,7 +1,159 @@
+x = sum([i if i%2==0 else 1 for i in range(20)])
+print(x)
 
+matrix = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [2, 2, 2],
+]
+flat = []
+for row in matrix:
+    for num in row:
+        flat.append(num)
+print(flat)
 
-s1="1997807002 -2043795113 -2099079149 1973797515 -1350798395 -1825032385 -1972063678 -1963964534 -1875193960 -1446233722 -1621579344 -1767950878 -1536417770 -1321606989 -1265101220 -1275360235 -1312592642 -1157823169 -1159301121 203444364 150391955 -953723537 -1126804126 -1140582236 -1094487437 78488623 -705555517 -529712827 -599850876 -565892074 -592020756 -81218148 -290023327 -343777789 52841161 195997869 349602904 299159762 222404073 273923204 382701968 365139865 459945223 412980487 1723755206 466859582 1678112047 586359208 536510306 692429543 674668841 1494269563 1484018517 1061438894 782398339 1061591341 1297791932 1375129367 1338721526 1645193603 1946170778 1731867490 2070978406 2114035725"
+def twoSum(nums, target):
+    l = len(nums)
+    index1 = 0
+    index2 = 0
+    for i in range(l):
+        index1 = i
+        left = target - nums[i]
+        for j in range(i+1,l):
+            if nums[j] == left:
+                index2 = j
+                return [index1,index2]
+print(twoSum([2,7,11,15],9))
 
-s2="1997807002 -2043795113 -2099079149 1973797515 -1350798395 -1825032385 -1972063678 -1963964534 -1875193960 -1446233722 -1621579344 -1767950878 -1536417770 -1321606989 -1265101220 -1275360235 -1312592642 -1157823169 -1159301121 203444364 150391955 -953723537 -1126804126 -1140582236 -1094487437 78488623 -705555517 -529712827 -599850876 -565892074 -592020756 -81218148 -290023327 -343777789 52841161 195997869 349602904 299159762 222404073 273923204 382701968 365139865 459945223 412980487 1723755206 466859582 1678112047 586359208 536510306 692429543 674668841 1494269563 1484018517 1061438894 782398339 1061591341 1297791932 1375129367 1338721526 1645193603 1946170778 1731867490 2070978406 2114035725"
+def isPalindrome(self, x):
+    s = str(x)
+    if len(s) == 2:
+        return s[0] == s[1]
+    mid = len(s) // 2
+    s1 = s[0:mid]
+    s2 = s[mid+1]
+    return s1 == s2[::-1]
 
-print(s1==s2)
+def removeElement(nums, val):
+    i = 0
+    for x in range(0,len(nums)):
+        if nums[x] != val:
+            nums[i] = nums[x]
+            i += 1
+    return nums
+
+def avg(path,sortby = ''):
+    with open(path) as file:
+        alist = file.read().splitlines() # used to split lines and remove newline character
+        lines = [l.split(',') for l in alist]
+    d={}
+    dname={}
+    dfinal = {}
+    for i in lines:
+        if d.get(i[1], -1) == -1:
+            d[i[1]] = [float(i[3])]
+            dname[i[1]] = i[0]
+        else:
+            d[i[1]].append(float(i[3]))
+    for k,v in d.items():
+        average = sum(v) / len(v)
+        dfinal[k] = dname[k],k,average,len(v)
+    wlines = [','.join(str(i) for i in tups) for tups in dfinal.values()]
+    print(wlines)
+    with open('result.txt', 'w') as wfile:
+        for i in range(len(wlines)):
+            if i + 1 == len(wlines):
+                wfile.write(wlines[i])
+            else:
+                wfile.write(wlines[i] + '\n')
+
+def insertmiddle(s1,s2):
+    l = len(s1)
+    mid = l//2
+    s3 = s1[:mid] + s2 + s1[mid:]
+    return s3
+
+def firstmiddlelast(s1,s2):
+    s3 = s1[0] + s2[0]
+    mid1 = len(s1)//2
+    mid2 = len(s2)//2
+    print(mid1)
+    s3 = s3 + s1[mid1] + s2[mid2] + s1[-1] + s2[-1]
+    return s3
+
+def lowercasefirst(s1):
+    upper = []
+    lower = []
+    for c in s1:
+        if c.islower():
+            lower.append(c)
+        else:
+            upper.append(c)
+    return ''.join(lower + upper)
+
+def countchardigitsymbol(s1):
+    upper = 0
+    lower = 0
+    dig = 0
+    symbol = 0
+    for c in s1:
+        if c.isalpha():
+            if c.islower():
+                lower += 1
+            else:
+                upper += 1
+        elif c.isdigit():
+            dig += 1
+        else:
+            symbol += 1
+    return upper,lower,dig,symbol
+
+def createmixedstring(s1,s2):
+    smallest = min([len(s1),len(s2)])
+    s3 = s2[::-1]
+    s4=''
+    r = 2
+    if len(s1) <= len(s2):
+        r = 1
+    for i in range(smallest):
+        s4 = s4 + s1[i] + s3[i]
+    if r == 1:
+        s4 = s4 + s2[i+1:]
+    else:
+        s4 = s4 + s1[i+1:]
+    return s4
+
+def balancestring(s1,s2):
+    l2 = [i for i in s1 if i not in s2]
+    return l2 == []
+
+def findnumoccurence(s1,s2):
+    s2 = s2.lower()
+    s1 = s1.lower()
+    j = 0
+    pointer = 0
+    num = 0
+    while 1:
+        pointer = s2.find(s1,pointer,len(s2))
+        if pointer > -1:
+            num += 1
+            pointer += len(s1)
+        else:
+            break
+    return num
+
+n = 785670
+count = 0
+while n > 0:
+    n = n // 10
+    count += 1
+print(count)
+
+num = 76542
+reverse_number = 0
+print("Given Number ", num)
+while num > 0:
+    reminder = num % 10
+    reverse_number = (reverse_number * 10) + reminder
+    num = num // 10
+print("Revered Number ", reverse_number)
