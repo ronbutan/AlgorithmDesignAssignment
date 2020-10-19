a41c569e8cf791392ad6f93c1169294073b3c780
Ron <ronbutan@yahoo.co.uk>
2020-10-19 15:50:07 +0800

    c

--- A3Q2.py
+++ A3Q2.py
@@ -9,35 +9,49 @@ def driving_mode(batt, num_seg, data):
     BATTERY_CHARGE = [seg[0]/PETROL_KM * seg[1] for seg in data]
     PETROL_COST = [seg[0]/PETROL_KM * PETROL_PRICE for seg in data]
     BATTERY_DRAIN = [-seg[0]/BATTERY_KM for seg in data]
-    # def driving_mode2(battLvl,mode,segNo,cost,battCharge,petrolCost,batteryDrain):
-    #     if segNo == len(battCharge):
-    #         if mode == 'p':
-    #             battLvl += battCharge[segNo-1]
-    #             cost += petrolCost[segNo-1]
-    #         else:
-    #             battLvl += batteryDrain[segNo-1]
-    #         return cost + max(0,batt-battLvl)
-        
-
-    #     if mode == 'p':
-    #         battLvl += battCharge[segNo-1]
-    #         cost += petrolCost[segNo-1]
-    #         if battLvl - 1 < MIN_BATT_LEVEL:
-    #             return driving_mode2(battLvl,'p',segNo+1,cost,battCharge,petrolCost,batteryDrain)
-    #         else:
-    #             return min(driving_mode2(battLvl,'p',segNo+1,cost,battCharge,petrolCost,batteryDrain),driving_mode2(battLvl-1,'b',segNo+1,cost,battCharge,petrolCost,batteryDrain))
-    #     else:
-    #         battLvl += batteryDrain[segNo-1]
-    #         if battLvl < MIN_BATT_LEVEL:
-    #             return driving_mode2(battLvl - 1,'p',segNo+1,cost,battCharge,petrolCost,batteryDrain)
-    #         else:
-    #             return min(driving_mode2(battLvl - 1,'p',segNo+1,cost,battCharge,petrolCost,batteryDrain),driving_mode2(battLvl,'b',segNo+1,cost,battCharge,petrolCost,batteryDrain))
+    PETROL_MODE = 'p'
+    BATTERY_MODE = 'b'
+    paths = []
+    costs = []
+    dp = [[float('inf') for seg in data] for seg in data]
 
     
-    # if batt - 1 >= MIN_BATT_LEVEL:
-    #     return min(driving_mode2(batt-1,'p',1,0,BATTERY_CHARGE,PETROL_COST,BATTERY_DRAIN),driving_mode2(batt-1,'b',1,0,BATTERY_CHARGE,PETROL_COST,BATTERY_DRAIN))
-    # else:
-    #     return driving_mode2(batt-1,'p',1,0,BATTERY_CHARGE,PETROL_COST,BATTERY_DRAIN)
+    def driving_mode2(battLvl,mode,segNo,cost,battCharge,petrolCost,batteryDrain,cumPath):
+        cumPath = cumPath + mode
+        if segNo == len(battCharge):
+            if mode == PETROL_MODE:
+                battLvl += battCharge[segNo-1]
+                cost += petrolCost[segNo-1]
+            else:
+                battLvl += batteryDrain[segNo-1]
+            paths.append(cumPath)
+            costs.append(cost + max(0,batt-battLvl))
+            return cost + max(0,batt-battLvl)
+        
+
+        if mode == PETROL_MODE:
+            battLvl += battCharge[segNo-1]
+            cost += petrolCost[segNo-1]
+            if battLvl - 1 < MIN_BATT_LEVEL:
+                return driving_mode2(battLvl,PETROL_MODE,segNo+1,cost,battCharge,petrolCost,batteryDrain,cumPath)
+            else:
+                return min(driving_mode2(battLvl,PETROL_MODE,segNo+1,cost,battCharge,petrolCost,batteryDrain,cumPath),driving_mode2(battLvl-1,BATTERY_MODE,segNo+1,cost,battCharge,petrolCost,batteryDrain,cumPath))
+        else:
+            battLvl += batteryDrain[segNo-1]
+            if battLvl < MIN_BATT_LEVEL:
+                return driving_mode2(battLvl - 1,PETROL_MODE,segNo+1,cost,battCharge,petrolCost,batteryDrain,cumPath)
+            else:
+                return min(driving_mode2(battLvl - 1,PETROL_MODE,segNo+1,cost,battCharge,petrolCost,batteryDrain,cumPath),driving_mode2(battLvl,BATTERY_MODE,segNo+1,cost,battCharge,petrolCost,batteryDrain,cumPath))
+
+    # driver code
+    if batt - 1 >= MIN_BATT_LEVEL:
+        c = min(driving_mode2(batt-1,PETROL_MODE,1,0,BATTERY_CHARGE,PETROL_COST,BATTERY_DRAIN,''),driving_mode2(batt-1,BATTERY_MODE,1,0,BATTERY_CHARGE,PETROL_COST,BATTERY_DRAIN,''))
+        print(paths[costs.index(c)])
+        return c
+    else:
+        c = driving_mode2(batt-1,PETROL_MODE,1,0,BATTERY_CHARGE,PETROL_COST,BATTERY_DRAIN,'')
+        print(paths[costs.index(c)])
+        return c
 
 num_line = int(sys.stdin.readline())
 for _ in range(num_line):
